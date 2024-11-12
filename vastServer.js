bb61ded96c5ddf84357fbaa787f9183a6a52a0ee
Michael Idundun <122805291+We13b-MD@users.noreply.github.com>
2024-11-12 10:48:02 +0100

    Update vastServer.js

--- vastServer.js
+++ vastServer.js
@@ -8,8 +8,8 @@
 
 
 
-const express = require('express')
-const cors = require('cors')
+import express from 'express';
+import cors = from 'cors
 const app = express()
 const port = 3000
 let impressionCount = 0;
